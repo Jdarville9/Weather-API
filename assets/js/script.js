d5a3e13ad505547c63c78442aa645ef95f416dad
Joseph Darville <joseph.darville9@gmail.com>
2022-09-21 03:42:42 -0500

    initiated the apocalypse

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const aScriptWithNoCode = () => {
+  console.log("I am a script with no code!");
+  initiateTheApocalypse();
+};
+
+const initiateTheApocalypse = () => {
+  console.log("The apocalypse has been initiated!");
+};
